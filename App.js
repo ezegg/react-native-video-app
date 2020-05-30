9c41251e692d83b1d47e47e93b4f587249786238
Ezequiel <ezeezegg@gmail.com>
2020-05-30 11:07:59 -0500

    added video component

--- App.js
+++ App.js
@@ -7,13 +7,14 @@
  */
 
 import React, {useEffect, useState, Component} from 'react';
-import {StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+import {StyleSheet, View, Text, StatusBar} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import CategoryList from './src/videos/containers/category-list';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Api from './utils/api';
+import Video from 'react-native-video';
 
 // const App: () => React$Node = () => {
 //   const [movies, setMovies] = useState({});
@@ -62,10 +63,18 @@ class App extends Component<Props> {
         <View style={styles.body}>
           <Home>
             <Header />
+            <View style={styles.containerVideo}>
+              <Video
+                source={{
+                  uri:
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                }}
+                style={styles.backgroundVideo}
+                resizeMode="contain"
+              />
+            </View>
             <CategoryList list={this.state.categoryList} />
             <SuggestionList list={this.state.suggestionList} />
-            <Text>Categorías</Text>
-            <Text>Sugerencias</Text>
           </Home>
         </View>
       </>
@@ -76,6 +85,18 @@ class App extends Component<Props> {
 const styles = StyleSheet.create({
   body: {
     backgroundColor: Colors.white,
+  },
+  containerVideo: {
+    flex: 1,
+    height: 100,
+    backgroundColor: 'red',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 
